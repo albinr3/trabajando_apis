73edb0969430108fdd9f60e2ffd62f10309966f1
Albin Rodriguez <albinmrodriguez@gmail.com>
2022-03-24 18:07:09 -0400

    m

--- main_oauth.py
+++ main_oauth.py
@@ -3,7 +3,7 @@ import pprint
 client_id= "8f4df31058da37c6653c"
 cliente_secret= "a15e7accac3334ede15bc89196b54e3f4ea1945d"
 code='16832ccea6c7428a025c'
-access_token= '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'
+
 
 if __name__=='__main__':
     
